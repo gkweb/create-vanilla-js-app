dc1157fe2eeaabbd0342bc91da39b81f95209708
Glade <gladekettle@gmail.com>
2018-07-15 18:10:24 +1000

    Init commit, draft vanilla js app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * The Enty point app
+ */
+export default class App {
+    constructor (options) {
+        
+    }
+
+    /**
+     * Init the app
+     */
+    init () {
+        
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+        }]
+    },
+    output: {
+        path: path.resolve(__dirname, 'public/assets/js/'),
+        filename: 'bundle.js'
+    }
+};
