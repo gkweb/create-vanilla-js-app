648b6f1de54a0ec93030038af8339371b8c3c01a
Glade <gladekettle@gmail.com>
2018-12-12 20:21:38 +1100

    semantics

--- test/test.js
+++ test/test.js
@@ -3,13 +3,13 @@ var path = require('path');
 var fs = require('fs');
 require('shelljs/global');
 
-beforeEach(async function() {
-  exec('rm -rf test/temp', {silent:true});
-  exec('mkdir test/temp', {silent:true});
-  exec('cd test/temp && cva demo1', {silent:true});
-});
-
 describe('CLI', function() {
+  beforeEach(async function() {
+    exec('rm -rf test/temp', {silent:true});
+    exec('mkdir test/temp', {silent:true});
+    exec('cd test/temp && cva demo1', {silent:true});
+  });
+
   it('Can generate app boilerplate', function() {      
     var generatedEntryPointJs = fs.readFileSync(__dirname + '/temp/demo1/src/index.js', 'utf-8')
 
