cd67799f2c3931cc5fbd752b7b24503692416897
Glade <gladekettle@gmail.com>
2018-12-12 20:11:16 +1100

    Basic mocha test for cli generation

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var path = require('path');
+var fs = require('fs');
+require('shelljs/global');
+
+beforeEach(async function() {
+  exec('rm -rf test/temp', {silent:true});
+  exec('mkdir test/temp', {silent:true});
+  exec('cd test/temp && cva demo1', {silent:true});
+});
+
+describe('CLI', function() {
+  it('Can generate app boilerplate globally', function() {      
+    var generatedEntryPointJs = fs.readFileSync(__dirname + '/temp/demo1/src/index.js', 'utf-8')
+
+    assert.equal((generatedEntryPointJs.length > 1), true);
+  });
+});
